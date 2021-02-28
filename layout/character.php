ddaa460fb258fe2a6c4855c9948d88801e58582b
My Digital School <MyDigitalSchool@MacBook-Pro-Deveosys.local>
2021-02-28 14:56:17 +0100

    Ajout du dossier layout

---
+++ layout/character.php
@@ -0,0 +1,8 @@
+<div>
+    <div class="card">
+        <div><?php echo "Id : " . $character->getId() ?></div>
+        <div><?php echo "Name : " . $character->getName() ?></div>
+        <div><?php echo "Life : " . $character->getLife() ?></div>
+        <div><?php echo "Mana : " . $character->getMana() ?></div>
+    </div>
+</div>
